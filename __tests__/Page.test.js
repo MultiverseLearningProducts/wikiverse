9160b7db2cc3cc934d878396874033c91ce2d75f
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-11-20 21:55:07 -0800

    wrote db sync for Page model test

--- __tests__/Page.test.js
+++ __tests__/Page.test.js
@@ -1,1 +1,7 @@
 const { db, Page } = require('../server/models/index.js');
+
+describe('------ Page model tests ------', () => {
+    beforeAll(async () => {
+        await db.sync({force: true});
+    });
+});
