8d9dbf1cf2a46f10a337565ad75c50c8afb9dd8d
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-11-20 21:54:19 -0800

    wrote db sync for Tag model test

--- __tests__/Tag.test.js
+++ __tests__/Tag.test.js
@@ -1,1 +1,7 @@
 const { db, Tag } = require('../server/models/index.js');
+
+describe('------ Tag model tests ------', () => {
+    beforeAll(async () => {
+        await db.sync({force: true});
+    });
+});
