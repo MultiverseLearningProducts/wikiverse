f59e31e8f20618bef85240f1d66fd6ff785f86cc
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-11-20 22:04:32 -0800

    wrote Page tests for READ and DELETE, Page tests finished for MVP

--- __tests__/Page.test.js
+++ __tests__/Page.test.js
@@ -5,4 +5,26 @@ describe('------ Page model tests ------', () => {
     beforeAll(async () => {
         await db.sync({force: true});
     });
+
+    it('can create a Page', async () => {
+        const newPage = await Page.create(pages[0]);
+        expect(newPage.title).toBe('First Page');
+        expect(newPage.content).toBe('This is my first page. I hope I love writing articles as much as I love reading them.');
+        expect(newPage.status).toBe('closed');
+        expect(newPage.authorId).toBe(2);
+    });
+
+    it('can delete a Page', async () => {
+        await Page.destroy({
+            where: {
+                id: 1
+            }
+        });
+
+        const notFound = await Page.findAll({
+            where: { id: 1 }
+        });
+
+        expect(notFound).toEqual([]);
+    });
 });
