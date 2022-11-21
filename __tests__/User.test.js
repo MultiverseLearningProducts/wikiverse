cf9bd26e4c24872440e92d7e6a7839678c121b32
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-11-20 21:53:34 -0800

    wrote db sync for User model test

--- __tests__/User.test.js
+++ __tests__/User.test.js
@@ -1,1 +1,7 @@
 const { db, User } = require('../server/models/index.js');
+
+describe('------ User model tests ------', () => {
+    beforeAll(async () => {
+        await db.sync({force: true});
+    });
+});
